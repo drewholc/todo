715cb34bd79bd0eaeb84934539abc0b92eedae7c
Drew Holcomb <Drew Holcomb>
2016-08-09 19:48:43 -0700

    first commit

---
+++ day01/assignment01.js
@@ -0,0 +1,6 @@
+//Drew Holcomb 08/08/16 JavaScript 1
+var a = 5;
+var b = 7;
+var c = a + b;
+
+alert(c);
---
+++ day01/script.js
@@ -0,0 +1,22 @@
+var name = 'Drew';
+var a = 5;
+var b = 6;
+var c = a + b;
+
+var e = true; //boolean values
+var f = false;
+
+var nothing = null; //nothing exists
+var something = undefined; //declared variable but nothing in it
+
+var arr = ['drew', 26, true];
+// console.log( name.length );
+
+
+var car = {
+  make: 'Ford',
+  model: 'Escape',
+  year: 2006
+};
+
+console.log( car.model );
