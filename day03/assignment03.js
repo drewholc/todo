d3a2ef6d1125fb432d7a5b207a23cfc4f97c6491
Drew Holcomb <Drew Holcomb>
2016-08-22 14:30:26 -0700

    day04

--- day03/assignment03.js
+++ day03/assignment03.js
@@ -3,5 +3,5 @@ var firstName = prompt('what is your first name?');
 function capitalizeFirstLetter(first) {
   return first.charAt(0).toUpperCase() + first.slice(1).toLowerCase();
 }
-var capped = capitalizeFirstLetter(firstName);
-console.log(capped);
+firstName = capitalizeFirstLetter(firstName);
+console.log(firstName);
